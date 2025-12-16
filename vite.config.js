4cfb362ec7655fec569948ed3a112a247a55b895
Rafael Frank <rafael.frank@deliveryhero.com>
2025-12-16 16:57:31 +0100

    Build and push

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  build: {
+    outDir: './docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
